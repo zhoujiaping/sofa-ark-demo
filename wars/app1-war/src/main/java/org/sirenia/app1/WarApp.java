9b789e86db7bd97324d711b5ce1b5445c3a8e043
zhoujiaping <421211679@qq.com>
2021-08-03 20:53:51 +0800

    feat: springboot as war file

---
+++ wars/app1-war/src/main/java/org/sirenia/app1/WarApp.java
@@ -0,0 +1,21 @@
+package org.sirenia.app1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @description
+ * @date 2021/08/03
+ */
+@SpringBootApplication
+public class WarApp extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WarApp.class);
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(WarApp.class, args);
+    }
+}
